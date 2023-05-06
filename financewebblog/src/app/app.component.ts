0c82c37a10199b6932d486636d04b882c38d4b8e
myinfo <chetraj0101@gmail.com>
2023-05-06 01:47:41 -0500

    added new pic and shapes design

--- financewebblog/src/app/app.component.ts
+++ financewebblog/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { format, addDays, differenceInDays } from 'date-fns';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'financewebblog';
+  currentTime: string | undefined;
+  //dateAfterTenDays: string;
+  //daysDifference: number;
+  timer: any;
+
+  constructor() {
+    //const currentDate = new Date();
+    //this.today = format(currentDate, 'PPpp');
+    //this.dateAfterTenDays = format(addDays(currentDate, 10), 'PPpp');
+    //this.daysDifference = differenceInDays(addDays(currentDate, 10), currentDate);
+  }
+ 
+
+  ngOnInit() {
+    const currentDate = new Date();
+    this.updateCurrentTime();
+    //this.dateAfterTenDays = format(addDays(currentDate, 10), 'Pp');
+    //this.daysDifference = differenceInDays(addDays(currentDate, 10), currentDate);
+
+    // Update the time every second (1000ms)
+    this.timer = setInterval(() => {
+      this.updateCurrentTime();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    // Clear the interval when the component is destroyed
+    clearInterval(this.timer);
+  }
+
+  updateCurrentTime() {
+    this.currentTime = format(new Date(), 'Pp');
+  }
+
+  
 }
