2af23e22b0c3fe8faafc0b89d6226ede2e8b1e68
myinfo <chetraj0101@gmail.com>
2023-05-08 00:36:49 -0500

    added currency api exchangerate

--- financewebblog/src/app/app.component.ts
+++ financewebblog/src/app/app.component.ts
@@ -2,29 +2,40 @@ import { Component } from '@angular/core';
 import { format, addDays, differenceInDays } from 'date-fns';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { HttpClient } from '@angular/common/http'; // import HttpClient
+import { interval } from 'rxjs';
 
 
+
+
+interface ExchangeRateData {
+  rates: { [key: string]: number};
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent {
-  
+
   title = 'financewebblog';
   currentTime: string | undefined;
   //dateAfterTenDays: string;
   //daysDifference: number;
   timer: any;
 
-  constructor() {
-    //const currentDate = new Date();
-    //this.today = format(currentDate, 'PPpp');
-    //this.dateAfterTenDays = format(addDays(currentDate, 10), 'PPpp');
-    //this.daysDifference = differenceInDays(addDays(currentDate, 10), currentDate);
-  }
- 
+
+  currencies = ['GBP','EUR','CHF', 'AUD', 'CAD', 'INR', 'JPY','CNY','NPR','NZD','SGD','KRW'];
+  currencyData: { [key: string]: any } = {};
+
+
+  constructor(private http: HttpClient) { } // add private http: HttpClient to the constructor
+
 
   ngOnInit() {
     const currentDate = new Date();
@@ -36,6 +47,11 @@ export class AppComponent {
     this.timer = setInterval(() => {
       this.updateCurrentTime();
     }, 1000);
+
+    // Call the getCurrencyData() method every second
+    interval(1000).subscribe(() => {
+      this.getCurrencyData();
+    });
   }
 
   ngOnDestroy() {
@@ -45,6 +61,35 @@ export class AppComponent {
 
   updateCurrentTime() {
     this.currentTime = format(new Date(), 'Pp');
-  } 
+  }
+
+
+  getCurrencyData() {
+    // Define the API endpoint and API key
+    const apiKey = 'bc5517787b5b49b5aab23f522877b04b';
+    const endpoint = 'https://openexchangerates.org/api/latest.json';
+    // Use the HttpClient service to retrieve the currency data for each currency
+    this.http.get<ExchangeRateData>(`${endpoint}?app_id=${apiKey}`).subscribe(data => {
+      this.currencies.forEach(currency => {
+        this.currencyData[currency] = data.rates[currency].toFixed(2);
+      });
+    });
+  }
+
+
+  /*
+  getCurrencyData() {
+    // Define the API endpoint and API key
+    const apiKey = 'bc5517787b5b49b5aab23f522877b04b';
+    const endpoint = 'https://openexchangerates.org/api/latest.json';
   
+    // Use the HttpClient service to retrieve the currency data for each currency
+    this.http.get(`${endpoint}?app_id=${apiKey}`).subscribe(data => {
+      this.currencies.forEach(currency => {
+        this.currencyData[currency] = data.rates[currency];
+      });
+    });
+  }
+  */
+
 }
